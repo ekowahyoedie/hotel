95eb2381c72c8e7c5be56a305485c392e446b8da
ekowahyoedie <ekowahyoedie702@gmail.com>
2026-05-14 03:59:59 +0800

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,8 @@
+def get_vals():
+    x = '2df9f5ee456d4346ae0077f21d7f89b2:2df9f5ee456d4346ae0077f21d7f89b2'
+    y = '2df9f5ee456d4346ae0077f21d7f89b3:2df9f5ee456d4346ae0077f21d7f89b3'
+    return x, y
+
+a, b = get_vals()
+print(a)
+print(b)
